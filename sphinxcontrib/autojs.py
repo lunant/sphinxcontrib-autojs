1174ac985754154ec89cae0fe2ca7f89bc4d3d39
Lee Heung-sub <heung@sublee.kr>
2010-09-27 14:50:36 +0900

    Add JavaScriptConsoleLexer into the extension.

--- sphinxcontrib/autojs.py
+++ sphinxcontrib/autojs.py
@@ -3,6 +3,8 @@ import os.path
 import re
 from docutils import nodes
 from docutils.statemachine import ViewList
+from pygments.lexers import JavascriptLexer, LEXERS
+from pygments.token import *
 from sphinx import addnodes
 from sphinx.domains.javascript import *
 from sphinx.util.compat import Directive
@@ -16,6 +18,49 @@ START = "/**"
 END = "*/"
 PROMPT = ">>> "
 CONTINUED = "... "
+
+
+class JavascriptConsoleLexer(JavascriptLexer):
+    """For Javascript console output or doctests, such as:
+
+    .. sourcecode:: jscon
+
+        >>> var a = "foo";
+        >>> a;
+        foo
+        >>> 1 / 0;
+        Infinity
+    """
+
+    name = "Javascript console session"
+    CONSOLE_RULES = [(r"(?:(?<=\n)|^)(>>>|\.\.\.)(?= )", Generic.Prompt),
+                     # for popular Javascript frameworks
+                     (r"\$|jQuery|MooTools|Class|Browser|Array|Function" \
+                      r"String|Hash|Event|Element|JSON|Cookie|Fx|Request",
+                      Name.Class)]
+    EXCEPTIONS = ["Error", "KeyError", "HTTPError", "ReferenceError"]
+    tokens = JavascriptLexer.tokens.copy()
+    tokens["root"] = CONSOLE_RULES + tokens["root"][:]
+    aliases = ["jscon"]
+    mimetypes = ["text/x-javascript-doctest"]
+    filenames = []
+    alias_filenames = []
+
+    def get_tokens_unprocessed(self, text):
+        is_example = False
+        is_output = False
+        for item in JavascriptLexer.get_tokens_unprocessed(self, text):
+            if item[1] is Generic.Prompt:
+                is_example = True
+                is_output = False
+            elif is_example and item[2].endswith(u"\n"):
+                is_example = False
+                is_output = True
+            elif is_output:
+                item = item[0], Generic.Output, item[2]
+            elif item[2] in self.EXCEPTIONS:
+                item = item[0], Name.Exception, item[2]
+            yield item
 
 
 class JSClassmember(JSCallable):
@@ -207,4 +252,5 @@ class AutoJavaScript(Directive):
 
 def setup(app):
     app.add_directive('autojs', AutoJavaScript)
+    app.add_lexer("jscon", JavascriptConsoleLexer())
 
